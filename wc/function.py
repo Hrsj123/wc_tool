8027bfae7418b0fa40835beebae4e61b81b2fb68
Harsh Vardhan <harshvardhan99679@gmail.com>
2024-03-23 21:28:40 +0530

    fix methods

--- wc/function.py
+++ wc/function.py
@@ -1,4 +1,4 @@
-import locale
+# import locale
 
 def num_of_bytes(text: bytes) -> str:
     """Returns the total bytes present in file"""
@@ -6,19 +6,30 @@ def num_of_bytes(text: bytes) -> str:
 
 def num_of_lines(text: bytes) -> str:
     """Returns the total number of lines used in a file"""
-    return str(len(text.decode('utf-8').split('\n')))
+    count = 0
+    for line in text.decode('utf-8').split('\n'):
+        count += 1 if line else 0
+    return str(count)
 
 def num_of_words(text: bytes) -> bytes:
     """Returns the total number of words used in a file"""
-    return str(len(text.decode('utf-8').split(' ')))
+    count = 0
+    for line in text.decode('utf-8').split('\n'):
+        count += len(line.split())
+    return str(count)
 
 def num_of_characters(text: bytes) -> str:
     """Returns the number of characters in a file"""
-    encoding = 'windows-1252' if locale.getlocale()[1] == '1252' else 'utf-8'
+    # encoding = 'windows-1252' if locale.getlocale()[1] == '1252' else 'utf-8'
     try:
-        res = 0
-        for char in text.decode(encoding):
-            res += len(char)
-    except UnicodeDecodeError:
+        res = len(text.decode('utf-8'))
+
+        ## Below code for locale based approach,
+        ## Python implicitly handles multibyte characters!
+
+        # res = 0
+        # for char in text.decode(encoding):
+        #     res += len(char)
+    except UnicodeDecodeError:     
         res = num_of_bytes(text)
     return str(res)
